fb218bf402319426ad800c13b532038adddfce91
GandalfTheGrey <lewis@infisoft.org.uk>
2013-06-01 09:09:54 +0100

    Rogues Den Cooker

---
+++ src/com/rsbot/roguescooker/Variables/Constants.java
@@ -0,0 +1,11 @@
+package com.rsbot.roguescooker.Variables;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Romi Grace
+ * Date: 01/06/13
+ * Time: 09:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class Constants {
+}
